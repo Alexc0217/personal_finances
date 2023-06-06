ce4d572bce4548a635afc194b450d1d7cf360ce4
alexc0217 <alex.costa.moura@outlook.com>
2023-06-05 23:33:59 -0300

    routes update

--- server/app/routes/user.js
+++ server/app/routes/user.js
@@ -6,6 +6,8 @@ router.get('/api/users', UserController.allUsers);
 router.get('/api/user/:id', UserController.findUser);
 router.post('/api/users', UserController.create);
 router.put('/api/user/:id', UserController.update);
+router.post('/api/user/:id/add-value', UserController.addValue);
+router.post('/api/user/:id/remove-value', UserController.removeValue);
 router.post('/api/users/login', UserController.login);
 
 module.exports = router;
