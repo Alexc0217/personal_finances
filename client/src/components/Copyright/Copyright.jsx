dedf7a72ed640e5987e9fc23779a59e103527afa
Christian Ricarte Rodrigues <62212826+christian-ricarte@users.noreply.github.com>
2023-06-22 20:58:20 -0300

    Ajuste na Label do Copyright

--- client/src/components/Copyright/Copyright.jsx
+++ client/src/components/Copyright/Copyright.jsx
@@ -8,7 +8,7 @@ export const Copyright = (props) => {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="#">
-          Personal Finances
+          Finance Friend
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
